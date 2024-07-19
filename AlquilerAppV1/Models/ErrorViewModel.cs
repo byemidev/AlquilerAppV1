03508c69cb6948fba7b9857767c51afe320bf107
byemidev <byemidev@gmail.com>
2024-07-19 18:04:15 +0200

    deleting

--- AlquilerAppV1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AlquilerAppV1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
