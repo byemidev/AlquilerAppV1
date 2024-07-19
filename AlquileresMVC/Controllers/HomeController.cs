f2ddd4b2d7870932887e386ef478841e91eef7b3
byemidev <byemidev@gmail.com>
2024-07-19 20:47:37 +0200

    addding changes

--- AlquileresMVC/Controllers/HomeController.cs
+++ AlquileresMVC/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AlquileresMVC.Models;
+using AlquileresMVC.Models.user;
+using System.Data.SqlClient;
 
 namespace AlquileresMVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly SqlConnection _connection; 
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly List<carDescription> _descriptions;
+    private readonly formRegistration _form;
+
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration config)
     {
         _logger = logger;
+        _connection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
     }
 
     public IActionResult Index()
@@ -27,5 +36,13 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    //custom logic
+    //controller 
+
+    public IActionResult carDescription() {
+        //TODO: send data from db
+        return View(_descriptions);
     }
 }
--- AlquileresMVC/Program.cs
+++ AlquileresMVC/Program.cs
@@ -3,7 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Configuration.GetConnectionString("DefaultConnection");
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
