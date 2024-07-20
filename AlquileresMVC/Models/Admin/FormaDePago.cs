aa56183c3b4427cea6154801b7522c4546c91b40
z0kr0m <noreply>
2024-07-20 23:00:59 +0100

    Agregar modelos

---
+++ AlquileresMVC/Models/Admin/FormaDePago.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlquileresMVC.Models.Admin
+{
+    public class FormaDePago
+    {
+        [Key]
+        public int FormaDePagoId { get; set; }
+        public int reservaId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
---
+++ AlquileresMVC/Models/Admin/Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlquileresMVC.Models.Admin
+{
+    public class Vehiculo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Modelo { get; set; } = string.Empty;
+        public string Marca { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public int precioDia { get; set; }
+    }
+}
